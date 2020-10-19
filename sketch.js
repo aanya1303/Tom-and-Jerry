f7a73a738770725e7e6ec3caf1e41ef3f0da50a3
aanya1303 <68226423+aanya1303@users.noreply.github.com>
2020-10-19 16:57:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+var tom,jerry;
+var player,game,form;
+var gameState,playerCount;
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(displayWidth-20 ,displayHeight-30);
+    database =firebase.database();
+    game=new Game();
+    game.getState();
+    game.start();
+}
+function draw(){
+    if(playerCount===2){
+        game.update(1);
+    }
+    if(gameState===1){
+        clear();
+        game.play();
+    }
+    if(gameState===2){
+        game.end();
+    }
+}
